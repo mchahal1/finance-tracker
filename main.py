165e470456cfe40d9719f6f347ce84b574413c37
mchahal1 <manveerc077@deltalearns.ca>
2025-01-31 14:39:42 -0800

    mostly complete

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 # Ask for monthly allowance and expenses in different categories
+
 answer = 0
 allowance = 0
 food = 0
 clothing = 0
 entertainment = 0
 
-allowance = input("whats ur allowance").int()
-food = input("How much did you spend on food?").int()
-clothing = input("How much did you spend on clothing?").int()
-entertainment = input("How much did you spend on entertainment?").int()
+allowance = int(input("whats ur allowance"))
+food = int(input("How much did you spend on food?"))
+clothing = int(input("How much did you spend on clothing?"))
+entertainment = int(input("How much did you spend on entertainment?"))
 # Calculate total expenses and remaining balance
-allowance - food - clothing - entertainment = answer
-print(answer)
+answer = allowance - food - clothing - entertainment
+
 # Display results
+print("Here's your balance:")
+print(answer)
+
 # Conditional message based on balance (depending on whether you overspent or underspent or spent just the right amount, write a specific message - try to be kind!)
+if answer > 0:
+    print("Good spending!")
+if answer == 0:
+    print("Juuust right. No more money left!")
+if answer < 0:
+    print("tough, cant be spending THAT much.")
